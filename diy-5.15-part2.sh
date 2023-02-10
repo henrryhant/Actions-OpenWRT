f150d368bc1081d0f815d2900f3b5d0ca63f9a55
henrryhant <60613880+henrryhant@users.noreply.github.com>
2023-02-11 07:50:42 +0800

    Update diy-5.15-part2.sh
    2023021102

--- diy-5.15-part2.sh
+++ diy-5.15-part2.sh
@@ -18,7 +18,7 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 
 ########### 更新smartdns版本 ###########
 sed -i "/^PKG_VERSION:=/cPKG_VERSION:=1.2022.40" feeds/packages/net/smartdns/Makefile
-sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=13d028df0dfdd422f9306b616fd65b47483ac9a1" feeds/packages/net/smartdns/Makefile
+sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=be71e085adde08ca46a76fa33bee6999cc9f28a9" feeds/packages/net/smartdns/Makefile
 sed -i "s/PKG_MIRROR_HASH/#PKG_MIRROR_HASH/" feeds/packages/net/smartdns/Makefile
 sed -i "51i     \$(INSTALL_DIR) \$(1)/etc/smartdns \$(1)/etc/smartdns/domain-set \$(1)/etc/smartdns/conf.d/" openwrt/feeds/packages/net/smartdns/Makefile
 ########### 更新adguardhome版本###########
