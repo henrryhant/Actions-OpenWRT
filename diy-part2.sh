ab26fe2fc36299e3216c3d2e508f937c480197b1
henrryhant <60613880+henrryhant@users.noreply.github.com>
2023-03-06 22:37:22 +0800

    Update diy-part2.sh
    202303062237

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,10 +19,9 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 ########### 更新smartdns版本 ###########
 rm -rf feeds/packages/net/smartdns
 git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
-#sed -i "/^PKG_VERSION:=/cPKG_VERSION:=1.2022.40" feeds/packages/net/smartdns/Makefile
-sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=b473b9c6e101f89ec302c7e080fa7f6bd59ca618" feeds/packages/net/smartdns/Makefile
-sed -i "s/PKG_MIRROR_HASH/#PKG_MIRROR_HASH/" feeds/packages/net/smartdns/Makefile
-#sed -i "51i     \$(INSTALL_DIR) \$(1)/etc/smartdns \$(1)/etc/smartdns/domain-set \$(1)/etc/smartdns/conf.d/" feeds/packages/net/smartdns/Makefile
+sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=3d0d24aacc4399a8e142e43f43993a340294a0f4" feeds/packages/net/smartdns/Makefile
+rm -rf feeds/luci/applications/luci-app-smartdns
+git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 ########### 更新adguardhome版本###########
 #rsync -rtv --delete package/greenice/adguardhome/ feeds/packages/net/adguardhome
 
