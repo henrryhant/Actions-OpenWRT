25911e4d0165d38d2bd0fbd8b51df47ec80fc289
henrryhant <60613880+henrryhant@users.noreply.github.com>
2023-03-11 14:19:47 +0800

    Update diy-part2.sh
    202303111419

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,11 +19,11 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 ########### 更新smartdns版本 ###########
 rm -rf feeds/packages/net/smartdns
 git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
-sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=1ac2b2ad98bef602bc7f4631ae3f84cb448fa4ee" feeds/packages/net/smartdns/Makefile
+sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=3c7b86eea755cf726a49d452a7b43ceeb01f2a06" feeds/packages/net/smartdns/Makefile
 rm -rf feeds/luci/applications/luci-app-smartdns
 git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 ########### 更新adguardhome版本###########
 #rsync -rtv --delete package/greenice/adguardhome/ feeds/packages/net/adguardhome
 
 ########### 更新xray-core版本###########
-rsync -rtv --delete package/greenice/xray-core/ feeds/packages/net/xray-core
+rsync -rtv --delete package/greenice/passwallpackages/xray-core/ feeds/packages/net/xray-core
