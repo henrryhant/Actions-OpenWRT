5430bce1f03db44abe378e0d176675331469a3ea
henrryhant <60613880+henrryhant@users.noreply.github.com>
2023-11-07 13:52:05 +0300

    Update diy-v2305-part2.sh

--- diy-v2305-part2.sh
+++ diy-v2305-part2.sh
@@ -19,19 +19,26 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 
 rm -rf package/network/services/omcproxy
 svn export https://github.com/greenice897/gipackages/trunk/omcproxy package/network/services/omcproxy
+rm -rf package/greenice/passwallpackages/sing-box
+rm -rf feeds/packages/net/sing-box
+svn export https://github.com/greenice897/gipackages/trunk/sing-box feeds/packages/net/sing-box
+svn export https://github.com/greenice897/gipackages/trunk/sing-box package/greenice/passwallpackages/sing-box
 
 ########### 更新smartdns版本 ###########
 rm -rf feeds/packages/net/smartdns
 git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
-# sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=aa6f6fd519592adb831b5c7a25583c574ddda2bf" feeds/packages/net/smartdns/Makefile
+sed -i "s/PKG_VERSION:=1.2023.42/PKG_VERSION:=1.2023.43/" feeds/packages/net/smartdns/Makefile
+sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=1ba6ee7cb98b5b6448bc2a2be318eb3518d4de79" feeds/packages/net/smartdns/Makefile
+sed -i "s/PKG_MIRROR_HASH/# PKG_MIRROR_HASH/" feeds/packages/net/smartdns/Makefile
 rm -rf feeds/luci/applications/luci-app-smartdns
 git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+# sed -i "s/PKG_VERSION:=1.2023.42/PKG_VERSION:=1.2023.43/" feeds/luci/applications/luci-app-smartdns/Makefile
 ########### 更新adguardhome版本###########
 #rsync -rtv --delete package/greenice/adguardhome/ feeds/packages/net/adguardhome
 
 ########### 更新xray-core版本###########
 rsync -rtv --delete package/greenice/passwallpackages/xray-core/ feeds/packages/net/xray-core
 rsync -rtv --delete package/greenice/passwallpackages/microsocks/ feeds/packages/net/microsocks
-rsync -rtv --delete package/greenice/passwallpackages/sing-box/ feeds/packages/net/sing-box
+# rsync -rtv --delete package/greenice/passwallpackages/sing-box/ feeds/packages/net/sing-box
 rsync -rtv --delete package/greenice/passwallpackages/v2ray-core/ feeds/packages/net/v2ray-core
 rsync -rtv --delete package/greenice/passwallpackages/v2ray-geodata/ feeds/packages/net/v2ray-geodata
